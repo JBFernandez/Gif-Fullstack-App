4d028090927ec373031473d3add4fd8472159fd1
Jose Fernandez <bernardo.fersan@gmail.com>
2023-04-06 13:35:51 -0600

    Initialized Spring Boot App

---
+++ server/loginApp/src/main/java/com/login/loginApp/LoginAppApplication.java
@@ -0,0 +1,13 @@
+package com.login.loginApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAppApplication.class, args);
+	}
+
+}
